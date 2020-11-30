19ec42055a0160aca1fb60bdb9809ca7e5e63c19
jorian <jorian@outlook.com>
2020-11-30 06:56:08 +0100

    initial key design

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,33 @@
+mod util {
+    mod key {
+        use secp256k1::{self, Secp256k1};
+
+        pub struct PrivateKey {
+            pub compressed: bool,
+            pub key: secp256k1::SecretKey
+        }
+
+        impl PrivateKey {
+            pub fn fmt_wif(&self) {
+                // KMD uses 160, BTC 128.
+                //
+            }
+
+            pub fn from_wif() {
+                // deserialize from base58
+                // check data.len()
+                // if 33, uncompressed,
+                // if 34, compressed
+            }
+        }
+
+        pub struct PublicKey {
+            pub compressed: bool,
+            pub key: secp256k1::PublicKey
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
