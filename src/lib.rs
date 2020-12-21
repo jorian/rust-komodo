3b1cb4b5b119435606582cb5918d54bf5228365e
jorian <jorian@outlook.com>
2020-12-21 07:34:55 +0100

    wip impl private key

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,16 @@
-mod util {
-    mod key {
+extern crate serde;
+// extern crate serde_json;
+// extern crate serde_test;
+
+pub mod util {
+    pub mod key {
+        use std::fmt::{self, Write};
         use secp256k1::{self, Secp256k1};
+        use bitcoin::util::base58;
+        use serde::Deserializer;
+        use serde::de::Visitor;
+        use bitcoin::hashes::core::fmt::Formatter;
+        use std::str::FromStr;
 
         pub struct PrivateKey {
             pub compressed: bool,
@@ -8,9 +18,21 @@ mod util {
         }
 
         impl PrivateKey {
-            pub fn fmt_wif(&self) {
-                // KMD uses 160, BTC 128.
-                //
+            pub fn public_key<C: secp256k1::Signing>(&self, secp: &Secp256k1<C>) -> PublicKey {
+                unimplemented!()
+            }
+
+            pub fn fmt_wif(&self, fmt: &mut dyn fmt::Write) -> fmt::Result {
+                let mut ret = [0; 34];
+                ret[0] = 160;
+                ret[1..33].copy_from_slice(&self.key[..]);
+                let privkey = if self.compressed {
+                    ret[33] = 1;
+                    base58::check_encode_slice(&ret[..])
+                } else {
+                    base58::check_encode_slice(&ret[..33])
+                };
+                fmt.write_str(&privkey)
             }
 
             pub fn from_wif() {
@@ -18,15 +40,81 @@ mod util {
                 // check data.len()
                 // if 33, uncompressed,
                 // if 34, compressed
+                unimplemented!()
             }
+        }
+
+        impl fmt::Display for PrivateKey {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                self.fmt_wif(f)
+            }
+        }
+
+        impl fmt::Debug for PrivateKey {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                write!(f, "[private key data]")
+            }
+        }
+
+        impl FromStr for PrivateKey {
+            type Err = bitcoin::util::key::Error;
+            fn from_str(s: &str) -> Result<PrivateKey, bitcoin::util::key::Error> {
+                PrivateKey::from_wif(s)
+            }
+        }
+
+        impl ::serde::Serialize for PrivateKey {
+            fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where
+                S: ::serde::Serializer {
+                serializer.collect_str(self)
+            }
+        }
+
+        impl<'de> ::serde::Deserialize<'de> for PrivateKey {
+            fn deserialize<D>(deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error> where
+                D: Deserializer<'de> {
+                struct WifVisitor;
+
+                impl<'de> ::serde::de::Visitor<'de> for WifVisitor {
+                    type Value = PrivateKey;
+
+                    fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+                        formatter.write_str("an ASCII WIF string")
+                    }
+
+                    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E> where
+                        E: ::serde::de::Error, {
+                        if let Ok(s) = ::std::str::from_utf8(v) {
+
+                        }
+                    }
+
+                    fn visit_bytes<E>(self, v: &[u8]) -> Result<Self::Value, E> where
+                        E: ::serde::de::Error, {
+                        if let Ok(s) = ::std::str::from_utf8(v) {
+                            PrivateKey::from_str(s).map_err(E::custom)
+                        }
+                    }
+                }
+        }
+
         }
 
         pub struct PublicKey {
             pub compressed: bool,
             pub key: secp256k1::PublicKey
         }
+
+        impl PublicKey {
+            pub fn to_wif(&self) -> String {
+                unimplemented!()
+            }
+        }
     }
 }
+
+pub use util::key::PublicKey;
+pub use util::key::PrivateKey;
 
 #[cfg(test)]
 mod tests {
